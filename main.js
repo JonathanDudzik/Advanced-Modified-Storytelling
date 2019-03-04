60fc11bbb4c6935d0f03c8bbd1079bed78079f93
JonDWesley <jonathan.d.wesley@gmail.com>
2019-03-04 07:09:30 -0500

    slider developed

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-function nextPanel(e) {
-    var elmnt = document.getElementById(e);
-    elmnt.scrollIntoView();
+// function nextPanel(e) {
+//     var elmnt = document.getElementById(e);
+//     elmnt.scrollIntoView();
+// }
+
+//selects all icon-buttons
+// const iconButtonsAll = document.querySelectorAll(".icon-container__button")
+//selects ONE icon button based on the data-attribute
+// const iconButton = document.querySelector(`figure[data-key="vca-audio"]`)
+
+function playAudio(e) {
+    const iconButton = e.srcElement.attributes[0].value;
+    const conceptMedia = document.querySelector(`audio[data-key="${iconButton}"]`)
+    const conceptElement = document.querySelector(`div[data-key="${iconButton}"]`)
+    const slides = document.querySelectorAll("div[class$='__show']");
+    if(!conceptElement) return;
+    if(conceptMedia) {conceptMedia.play()};
+    
+    slides.forEach(function(slide) {
+        slide.classList.remove('vca-container__show');
+    });
+    
+    conceptElement.classList.add('vca-container__show');
 }
+
+
+// function playAudio(e) {
+//     const audio = document.querySelector(`audio[data-audio="vca-audio"]`)
+//     const vcaAudioDisplay = document.querySelector(`div[data-audio="vca-audio"]`)
+//     vcaAudioDisplay.classList.add('vca-container__show');
+//     audio.play();
+// }
+
+document.addEventListener('click', playAudio);
 
 // function playSound(e) {
 //     //is there a data-key in an audio element that matches the keyCode that is down
